6a8f9f50bc041ae55ac2ef00b6edc2ccee0c1542
Edwin Joy <edwin.joy@incoresemi.com>
2025-10-18 07:07:51 +0000

    convert indents to spaces

--- src/rtl/clk_timer.v
+++ src/rtl/clk_timer.v
@@ -5,9 +5,9 @@
 * This clock is assumed to tick every 1ms.
 */
 module clk_timer(
-		output wire	clk;
-		output wire [32:0] time32
-		);
+        output wire	clk,
+        output wire [32:0] time32
+        );
 
     // internal registers that stores state
     reg r_clk;
@@ -15,8 +15,8 @@ module clk_timer(
 
      // initialize registers
     initial begin
-    	r_clk = 1'b0;
-    	r_time32 = 32'b0;
+        r_clk = 1'b0;
+        r_time32 = 32'b0;
     end
 
     // assume clk toggles every 1ms for #1.
