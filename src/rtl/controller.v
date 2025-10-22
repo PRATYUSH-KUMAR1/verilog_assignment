e116bda6f7b5838b39a65ddf91d562c660961b73
PRATYUSH-KUMAR1 <kumarpartyush563@gmail.com>
2025-10-23 00:50:24 +0530

    Updated controller.v with changes

--- src/rtl/controller.v
+++ src/rtl/controller.v
@@ -1,97 +1,146 @@
+`timescale 1ms/1ms
+
 /*
-    Here we are doing only for the trafic where the
-    1: East-west = S0;
-    2: MR-PMC = S1;
-    3: OBH-PMC = S2;
-    Green=g; Yellow= y; Red=r or all red;
-*/
-module controller(
-        input wire clk,
-        input wire [32:0] time32,
-        output S
-     );
+  here we are doing only for the trafic where the 
+   1: East-west = S0;
+   2: MR-PMC = S1;
+   3: OBH-PMC = S2;
+   Green=g; Yellow= y; Red=r or all red;
+   */
 
-    reg [1:0] state, nextstate, light, next_light;	// register to store the present and next state;
+module state(
+	input wire clk,
+	input wire [1:0] B,
+	input wire [1:0] V,
+    input wire [32:0] time32,
+	output reg [2:0] S
+ 		);
+	
+	reg [2:0] state, nextstate;
+	reg [1:0] light, next_light;	// register to store the present and next state;
 
-    clk_timer U0(.clk(clk),.time32(time32[32:0]);		 // calling the clock;
+	
+	
 
-    parameter S0 = 3'b101; 					// defining the states in the binary;
-    parameter S1 = 3'b011;
-    parameter S2 = 3'b110;
-    parameter G0 = 2'b00;				// this is considering the light colours as a state
-    parameter Y0 = 2'b01;
-    parameter R0 = 2'b10;
-
-    parameter g1 = 20;	//seting the time for green,yellow,red;
-    parameter g2 = 10;
-    parameter y = 4;
-    parameter r = 2;
-
-    //* here i have to call the clock that is designed in the module clk_timer.
-    always @ (posedge clk, posedge reset)
-        if (reset) state <= S0;
+	
+	always @(posedge clk) begin
+		if(B==1)
+			state <= S3;
+		else if(V==0)
+			state <= S3;
+		else if(B==0 & V==1)    
+			state <= S0;
         else state <= nextstate;
+		end
 
-    always @(*)
-        case (state)
-            S0: begin
-                case(light)
-                    G0: begin
-                    if(time32==g1) begin
-                        next_light = Y0;
-                        end
-                    end
-                    Y0: begin
-                        if(time32==y) begin
-                            next_light = R0;
-                        end
-                        end
-                    R0: begin
-                        if(time32==r) begin
-                               nextstate = S1;
-                        end
-                    end
-                endcase
-            end
-            S1:begin
-                case(light)
-                    G0: begin
-                        if(time32==g2) begin
-                            next_light = Y0;
-                            end
-                        end
-                    Y0: begin
-                        if(time32==y) begin
-                            next_light = R0;
-                            end
-                        end
-                    R0: begin
-                        if(time32==r) begin
-                        nextstate = S2;
-                            end
-                        end
-                endcase
-               end
-               S2: begin
-                  case(light)
-                      G0: begin
-                          if(time32==g2) begin
-                             next_light = Y0;
-                             end
-                        end
-                    Y0: begin
-                        if(time32==y) begin
-                            next_light = R0;
-                            end
-                        end
-                    R0: begin
-                        if(time32==r) begin
-                            nextstate = S0;
-                            end
-                        end
-                    endcase
-                end
-            default: nextstate = S0;
-        endcase
-    assign S = (state==S0);
+	parameter S0 = 3'b101; 					// defining the states in the binary;
+	parameter S1 = 3'b011;
+	parameter S2 = 3'b110;
+
+	parameter S3 = 3'b111;					// adding the padesrerian sequence
+
+	parameter G0 = 2'b00;				// this is considering the light colours as a state
+	parameter Y0 = 2'b01;
+	parameter R0 = 2'b10;
+
+	
+	parameter g1 = 20;	//seting the time for green,yellow,red;
+	parameter g2 = 10;
+	parameter y = 4;
+	parameter r = 2;
+	
+	parameter gp = 10;			// the time sequence for the pedesterian
+	parameter yp =6;
+	
+	initial begin
+    state = S0;
+    light = G0;
+	end
+
+	
+
+	always @(*)
+			case (state)
+				S0: begin
+					case(light)
+						G0: begin
+						if(time32==g1) begin
+							next_light = Y0;
+							end	
+						end
+						Y0: begin
+							if(time32==y) begin
+								next_light = R0;
+							end
+							end
+						R0: begin
+							if(time32==r) begin
+								nextstate = S1;
+							end
+						end
+					endcase
+				end
+				S1:begin
+					case(light)
+						G0: begin
+							if(time32==g2) begin
+								next_light = Y0;
+								end
+							end
+						Y0: begin
+							if(time32==y) begin
+								next_light = R0;
+								end
+							end
+						R0: begin
+							if(time32==r) begin
+							nextstate = S2;
+								end
+							end
+					endcase
+				end
+				S2: begin
+					case(light)
+						G0: begin
+							if(time32==g2) begin
+								next_light = Y0;
+								end
+							end
+						Y0: begin 
+							if(time32==y) begin
+								next_light = R0;
+								end
+							end
+						R0: begin
+							if(time32==r) begin
+								nextstate = S0;
+								end
+							end
+						endcase
+					end	""
+				S3: begin
+					case(light)
+						G0: begin
+						if(time32==gp) begin
+							next_light = Y0;
+							end	
+						end
+						Y0: begin
+							if(time32==yp) begin
+								next_light = R0;
+							end
+							end
+						R0: begin
+							if(time32==r) begin
+								nextstate = S0;
+							end
+						end
+					endcase
+				end	
+				default: nextstate = S0;
+			endcase
+	always @(*) begin
+        S = state;
+    end
+
 endmodule
