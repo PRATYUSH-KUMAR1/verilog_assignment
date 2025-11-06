ae801b3b94e64215b854aaefd3e4b5e921430849
PRATYUSH-KUMAR1 <kumarpartyush563@gmail.com>
2025-11-06 14:44:41 +0530

    test-but fail

--- src/testbench/controller_tb.v
+++ src/testbench/controller_tb.v
@@ -1,20 +1,20 @@
 `timescale 1ms/1ms
 
-module controller_tb(
-   
-    reg clk,
-    reg [1:0] B, V,
-    wire [32:0] time32
-    wire [2:0] S);
+module controller_tb;
 
-   
-    clk_timer CLK1(
+    reg [1:0] B, V;
+    wire clk;
+    wire [32:0] time32;
+    wire [2:0] S;
+
+    // instantiate the clock generator
+    clk_timer CLK1 (
         .clk(clk),
         .time32(time32)
     );
 
- 
-    controller U0(
+    // instantiate the traffic controller
+    controller U0 (
         .clk(clk),
         .B(B),
         .V(V),
@@ -22,22 +22,21 @@ module controller_tb(
         .S(S)
     );
 
-   
+    // test sequence
     initial begin
-     
-        $dumpfile("traffic.vcd");   
-        $dumpvars(0, controller_tb);         
+        $dumpfile("traffic.vcd");
+        $dumpvars(0, controller_tb);
 
-        
-        B = 0; V = 1; 
-        #5000;                     
-        B = 1; V = 1;                     
-        #2000;
-        B = 0; V = 0;            
-        #2000;               
-        B = 1; V = 0;                     
-        #3000;                
+        B = 0; V = 1;
+        #50000;
+        B = 1; V = 1;
+        #20000;
+        B = 0; V = 0;
+        #80000;
+        B = 1; V = 0;
+        #70000;
 
-        $finish;                   
+        $finish;
     end
+
 endmodule
