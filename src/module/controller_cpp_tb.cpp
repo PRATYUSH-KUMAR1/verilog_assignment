93e9e1664c6d0a82d13b7c5155b6ccd3381dda1e
PRATYUSH-KUMAR1 <kumarpartyush563@gmail.com>
2025-12-22 16:02:55 +0530

    mod

--- src/module/controller_cpp_tb.cpp
+++ src/module/controller_cpp_tb.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <string>
+// #include <string>
 // #include <chronos>
 #include <bitset>
-// #include 
+#include <cstdint>
 
 enum class STATE{
      S_EW,
@@ -53,12 +53,20 @@ enum class LIGHT_STATE{
 //                 bool B = false;            
 //             };
 
-void controller(SENSOR_IO &sensor){
+void controller(SENSOR_IO &sensor, uint32_t timer){
     
-    STATE state = STATE::S_EW;
-    LIGHT_STATE light = LIGHT_STATE::VCL_GREEN;
+    static STATE state = STATE::S_EW;
+    static LIGHT_STATE light = LIGHT_STATE::VCL_GREEN;
 
-    int i=0; 
+    // FOLLOWING IS FOR TIME TRACKING
+    static uint32_t state_entery_time = 0;
+    uint32_t i = timer - state_entery_time;
+
+    // CREATING THE PEDESTERIAN LATCH THAT NOTE THE BOTTON INPUTS
+    static bool ped_latched = false;
+    if ((sensor.B.any()) && state != STATE::S_PED)      // IF (ANY PEDESTERIAN BOTTON IS PRESSED "AND" THE PEDESTERIAN SERVICE IS NOT READY);
+        ped_latched = true;
+
     // DEFINING THE LIGHT TIME PERIOD
     int ew_green = 20;
     int green = 10;
@@ -70,69 +78,126 @@ void controller(SENSOR_IO &sensor){
     switch (state){
         case STATE::S_EW:
 
+            if (!sensor.V2){
+                state = STATE::S_MR;
+                state_entery_time = timer;
+                break;
+            }
+
             switch (light){
                 case LIGHT_STATE::VCL_GREEN:
-                        if((i>= ew_green) || (sensor.V2 == 0))
-                        {light = LIGHT_STATE::VCL_YELLOW;}
+                        if((i>= ew_green) || (!sensor.V2))
+                        {
+                            light = LIGHT_STATE::VCL_YELLOW;
+                            state_entery_time = timer;
+                        }
                         break;
                 case LIGHT_STATE::VCL_YELLOW:
-                    if((i>= yellow) || (sensor.V2 == 0))
-                    {light = LIGHT_STATE::ALL_RED;}
+                    if((i>= yellow) || (!sensor.V2))
+                    {
+                        light = LIGHT_STATE::ALL_RED;
+                        state_entery_time = timer;
+                    }
                     break;
                 case LIGHT_STATE::ALL_RED:
-                    if((i>= all_red))
-                        if(sensor.B1)
-                        {state = STATE::S_PED;
-                        light = LIGHT_STATE::PED_GREEN;}
+                    if((i>= all_red)){
+                        if(ped_latched)
+                        {
+                            state = STATE::S_PED;
+                            light = LIGHT_STATE::PED_GREEN;
+                        }
                             else 
-                            {state = STATE::S_MR;
-                            light = LIGHT_STATE::VCL_GREEN;}
+                            {
+                                state = STATE::S_MR;
+                                light = LIGHT_STATE::VCL_GREEN;
+                            }
+                            break;
+                            state_entery_time = timer;
+                        }
                     break;
            } 
         break;
 
         case STATE::S_MR:
 
+           if (!sensor.V1){
+                state = STATE::S_OBH;
+                state_entery_time = timer;
+                break;
+           }
+
             switch (light){
                 case LIGHT_STATE::VCL_GREEN:
-                        if((i>= green) || (sensor.V1 == 0))
-                        {light = LIGHT_STATE::VCL_YELLOW;}
+                        if((i>= green) || (!sensor.V1))
+                        {
+                            light = LIGHT_STATE::VCL_YELLOW;
+                            state_entery_time = timer;
+                        }
                         break;
                 case LIGHT_STATE::VCL_YELLOW:
-                    if((i>= yellow) || (sensor.V1 == 0))
-                    {light = LIGHT_STATE::ALL_RED;}
+                    if((i>= yellow) || (!sensor.V1))
+                    {
+                        light = LIGHT_STATE::ALL_RED;
+                        state_entery_time = timer;
+                    }
                     break;
                 case LIGHT_STATE::ALL_RED:
-                    if((i>= all_red))
-                        if(sensor.B1)
-                        {state = STATE::S_PED;
-                        light = LIGHT_STATE::PED_GREEN;}
+                    if((i>= all_red)){
+                        if(ped_latched)
+                        {
+                            state = STATE::S_PED;
+                            light = LIGHT_STATE::PED_GREEN;
+                        }
                         else 
-                            {state = STATE::S_OBH;
-                            light = LIGHT_STATE::VCL_GREEN;}
+                            {
+                                state = STATE::S_OBH;
+                                 light = LIGHT_STATE::VCL_GREEN;
+                            }
+                            break;
+                            state_entery_time = timer;
+                        }
                     break;
            }
         break;
 
         case STATE::S_OBH:
+           
+           if (!sensor.V0){
+            state = STATE::S_EW;
+            state_entery_time = timer;
+            break;
+           }
 
             switch (light){
                 case LIGHT_STATE::VCL_GREEN:
-                        if((i>= green) || (sensor.V0 == 0))
-                        {light = LIGHT_STATE::VCL_YELLOW;}
+                        if((i>= green) || (!sensor.V0))
+                        {
+                            light = LIGHT_STATE::VCL_YELLOW;
+                            state_entery_time = timer;
+                        }
                         break;
                 case LIGHT_STATE::VCL_YELLOW:
-                    if((i>= yellow) || (sensor.V0 == 0))
-                    {light = LIGHT_STATE::ALL_RED;}
+                    if((i>= yellow) || (!sensor.V0))
+                    {
+                        light = LIGHT_STATE::ALL_RED;
+                        state_entery_time = timer;
+                    }
                     break;
                 case LIGHT_STATE::ALL_RED:
-                    if((i>= all_red))
+                    if((i>= all_red)){
                         if(sensor.B1)
-                        {state = STATE::S_PED;
-                        light = LIGHT_STATE::PED_GREEN;}
+                        {
+                            state = STATE::S_PED;
+                            light = LIGHT_STATE::PED_GREEN;
+                        }
                         else 
-                            {state = STATE::S_EW;
-                            light = LIGHT_STATE::VCL_GREEN;}
+                            {
+                                state = STATE::S_EW;
+                                light = LIGHT_STATE::VCL_GREEN;
+                            }
+                            break;
+                            state_entery_time = timer;
+                    }
                     break;
            }
         break;
@@ -141,23 +206,34 @@ void controller(SENSOR_IO &sensor){
 
             switch (light){
                 case LIGHT_STATE::PED_GREEN:
-                        if((i>= green_ped) || (sensor.V2==0) || (sensor.V1==0) || (sensor.V0==0))
-                        {light = LIGHT_STATE::PED_YELLOW;}
-                        else 
-                            {light = LIGHT_STATE::PED_GREEN;}
+                        if(i>= green_ped)
+                        {
+                            light = LIGHT_STATE::PED_YELLOW;
+                            state_entery_time = timer;
+                        }
                         break;
                 case LIGHT_STATE::PED_YELLOW:
                     if((i>= yellow_ped))
-                    {light = LIGHT_STATE::ALL_RED;}
+                    {
+                        light = LIGHT_STATE::ALL_RED;
+                        state_entery_time = timer;
+                    }
                     break;
                 case LIGHT_STATE::ALL_RED:
-                    if((i>= all_red))
-                        if((sensor.V2==0) || (sensor.V1==0) || (sensor.V0==0))
-                        {state = STATE::S_PED;
-                        light = LIGHT_STATE::PED_GREEN;}
-                        else 
-                            {state = STATE::S_EW;
-                            light = LIGHT_STATE::VCL_GREEN;}
+                    if((i>= all_red)){
+                        ped_latched = false;                // RESET THE PEDESTERIAN BOTTON
+
+                            if((!sensor.V2) && (!sensor.V1) && (!sensor.V0))        // IF THERE IS NO VEHICAL AT ANY ROAD THEN
+                            {state = STATE::S_PED;
+                            light = LIGHT_STATE::PED_GREEN;}
+                            else 
+                                {
+                                    state = STATE::S_EW;
+                                    light = LIGHT_STATE::VCL_GREEN;
+                                    state_entery_time = timer;
+                                }
+                            break;
+                    }
                     break;
            }
         break;    
